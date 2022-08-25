17b49ef01cd827489b1b465d4c6174d7d9cd0cdd
Kristina <kuukristina@gmail.com>
2022-08-25 10:08:08 -0400

    Added ArrayList1 class

---
+++ src/second/ArrayList1.java
@@ -0,0 +1,11 @@
+package second;
+import java.util.ArrayList;
+
+public class ArrayList1 {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> list  = new ArrayList<>();
+    }
+}
